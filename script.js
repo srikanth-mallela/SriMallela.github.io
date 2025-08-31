964887322840af8d8b0f69684d6a6d2872fa5ab3
srikanth-mallela <113015837+srikanth-mallela@users.noreply.github.com>
2025-08-31 09:29:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('clickMe').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'Hello! You clicked the button!';
-});
+//document.getElementById('clickMe').addEventListener('click', function() {
+//    document.getElementById('message').textContent = 'Hello! You clicked the button!';
+//});
